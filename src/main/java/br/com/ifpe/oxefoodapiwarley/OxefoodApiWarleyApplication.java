f8e31c0515795c7ac3922966484bbfbc02aa6d1e
warlxy <warleysilva.dev@gmail.com>
2024-02-27 16:23:26 -0300

    inicializacao do projeto

---
+++ src/main/java/br/com/ifpe/oxefoodapiwarley/OxefoodApiWarleyApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodapiwarley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiWarleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiWarleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxefoodapiwarley/OxefoodApiWarleyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodapiwarley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodApiWarleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
