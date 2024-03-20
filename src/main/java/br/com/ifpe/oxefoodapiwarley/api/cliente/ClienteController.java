d48119e973c31e2429635097d898304439aa448e
aluno <>
2024-03-20 20:04:33 -0300

    feat: implementação do controller

--- src/main/java/br/com/ifpe/oxefoodapiwarley/api/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefoodapiwarley/api/cliente/ClienteController.java
@@ -3,14 +3,12 @@ package br.com.ifpe.oxefoodapiwarley.api.cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.ifpe.oxefoodapiwarley.modelo.cliente.Cliente;
 import br.com.ifpe.oxefoodapiwarley.modelo.cliente.ClienteService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cliente")
@@ -26,4 +24,15 @@ public class ClienteController {
         Cliente cliente = clienteService.save(request.build());
         return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
     }
+
+    @GetMapping
+    public List<Cliente> listarTodos() {
+        return clienteService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Cliente obterPorID(@PathVariable Long id) {
+        return clienteService.obterPorID(id);
+    }
+
 }
