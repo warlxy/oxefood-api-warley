237fc77523f9c5d13e39ff5af61c790bf5a941e6
aluno <>
2024-03-20 20:06:07 -0300

    feat: implementação do service

--- src/main/java/br/com/ifpe/oxefoodapiwarley/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefoodapiwarley/modelo/cliente/ClienteService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class ClienteService {
@@ -20,5 +21,16 @@ public class ClienteService {
         cliente.setDataCriacao(LocalDate.now());
         return repository.save(cliente);
     }
+
+    public List<Cliente> listarTodos() {
+
+        return repository.findAll();
+    }
+
+    public Cliente obterPorID(Long id) {
+
+        return repository.findById(id).get();
+    }
+
 
 }
